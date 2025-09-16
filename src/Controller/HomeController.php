cf46b5e20af478eda7814ae07f8aab7db8163aaf
PaulineBr0d <briaud.pauline@gmail.com>
2025-09-16 13:16:08 +0200

    update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class HomeController extends AbstractController
 {
